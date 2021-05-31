374314bce2ea39cfb02b6bcd80ef48d8cbea7836
Nidhijhabbe <jhabbenidhi1998@gmail.com>
2021-05-31 20:53:18 +0530

    Notification

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.notification;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+    }
+
+    public void sendOnChannel1(View v){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+
+
+    }
+
+    public void sendOnChannel2(View v){
+
+    }
+}
